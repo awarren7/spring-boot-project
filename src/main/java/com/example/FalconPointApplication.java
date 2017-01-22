bed84af48bc3f05cf27bc1d16006de74416803a0
Developer <andre_warren@yahoo.com>
2017-01-22 16:48:31 -0500

    fourth commit

--- src/main/java/com/example/FalconPointApplication.java
+++ src/main/java/com/example/FalconPointApplication.java
@@ -8,6 +8,6 @@ public class FalconPointApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FalconPointApplication.class, args);
-		System.out.println("hello");
+		System.out.println("money honey");
 	}
 }
